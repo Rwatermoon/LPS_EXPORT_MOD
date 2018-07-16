38544fb02e5437962cadbf192eaa44e3f2336fab
Rwatermoon <Rwatermoon@gmail.com>
2018-07-16 16:25:41 +0800

    Add Catch Excepation Add Option detail

--- FilledForm.py
+++ FilledForm.py
@@ -16,86 +16,129 @@ class FilledForm:
         self.filled_question_qid=defaultdict(list)
 
     def init_form(self,database_info,table_name):
-        conn = mysql.connector.connect(host=database_info['host'], user=database_info['user'],
-                                       password=database_info['password'],
-                                       database=database_info['database'], use_unicode=True)
-        cursor = conn.cursor()
+        try:
+            conn = mysql.connector.connect(host=database_info['host'], user=database_info['user'],
+                                           password=database_info['password'],
+                                           database=database_info['database'], use_unicode=True)
+            cursor = conn.cursor()
 
-        logging.info("Success connect to {}".format(database_info['host']))
-        # Query form_id
-        sql_str = 'select form_id from {} where id={}'.format(table_name['filled_form'], self.ini_form_id)
+            logging.info("Success connect to {}".format(database_info['host']))
+            # Query form_id
+            sql_str = 'select form_id from {} where id={}'.format(table_name['filled_form'], self.ini_form_id)
 
-        cursor.execute(sql_str)
-        template_from_id = cursor.fetchone()
-        if template_from_id is not None:
-            template_from_id = template_from_id[0]
-        else:
-            return None
+            cursor.execute(sql_str)
+            template_from_id = cursor.fetchone()
+            if template_from_id is not None:
+                template_from_id = template_from_id[0]
+            else:
+                return None
 
-        # Query form_info
-        sql_str = 'select content from {} where id={}'.format(table_name['form_info'], template_from_id)
-        cursor.execute(sql_str)
-        from_content = cursor.fetchone()
-        if from_content is not None:
-            from_content = from_content[0]
-        else:
-            return None
-
-
-        content_json = json.loads(from_content)
-        self.sections = content_json['sections']
-        self.questions = content_json['questions']
+            # Query form_info
+            sql_str = 'select content from {} where id={}'.format(table_name['form_info'], template_from_id)
+            cursor.execute(sql_str)
+            from_content = cursor.fetchone()
+            if from_content is not None:
+                from_content = from_content[0]
+            else:
+                return None
 
 
+            content_json = json.loads(from_content)
+            self.sections = content_json['sections']
+            self.questions = content_json['questions']
 
-        for sec_q in self.questions:
-            self.filled_question_qid[sec_q['qid']]=[]
 
-        logging.debug("Success load all data")
-        conn.close()
+
+            for sec_q in self.questions:
+                self.filled_question_qid[sec_q['qid']]=[]
+
+            logging.debug("Success load all data")
+        except AttributeError as e:
+            print(e)
+            logging.info("{} in init_form".format(e))
+        finally:
+            conn.close()
 
     def add_form(self,form_id,database_info,table_name):
-        conn = mysql.connector.connect(host=database_info['host'], user=database_info['user'],
-                                       password=database_info['password'],
-                                       database=database_info['database'], use_unicode=True)
-        cursor = conn.cursor()
-        sql_str = 'select * from {} where filled_id={}'.format(table_name['filled_question'], form_id)
-        cursor.execute(sql_str)
-        filled_questions = cursor.fetchall()
-        for q in filled_questions:
-            self.filled_question_qid[q[3]].append(q)
+        try:
+            conn = mysql.connector.connect(host=database_info['host'], user=database_info['user'],
+                                           password=database_info['password'],
+                                           database=database_info['database'], use_unicode=True)
+            cursor = conn.cursor()
+            sql_str = 'select * from {} where filled_id={}'.format(table_name['filled_question'], form_id)
+            cursor.execute(sql_str)
+            filled_questions = cursor.fetchall()
+            for q in filled_questions:
+                self.filled_question_qid[q[3]].append(q)
 
-        logging.info("Success read form with ID={}".format(form_id))
-        conn.close()
+            logging.info("Success read form with ID={}".format(form_id))
+        except AttributeError as e:
+            print(e)
+            logging.info("{} in add_form".format(e))
+        finally:
+            conn.close()
 
     def save_form(self,save_path):
 
-        wb = xlwt.Workbook(encoding='utf-8')
-        for section in self.sections:
-            ws = wb.add_sheet(section['title'], cell_overwrite_ok=True)
-            sec_questions = [item for item in self.questions if item['qid'] // 100 == section['sid']]
+        try:
+            wb = xlwt.Workbook(encoding='utf-8')
+            for section in self.sections:
+                ws = wb.add_sheet(section['title'], cell_overwrite_ok=True)
+                sec_questions = [item for item in self.questions if item['qid'] // 100 == section['sid']]
 
-            line_num = 0
-            for sec_question in sec_questions:
-                row_num = 0
+                line_num = 0
+                for sec_question in sec_questions:
+                    row_num = 0
 
-                ws.write(row_num,line_num,sec_question['title'])
-                row_num+=1
-                if sec_question['type']=='table':
-                    print(sec_question['type'])
-                    for table_item in sec_question['extras']:
-                        print(table_item)
-                        ws.write(row_num,line_num,table_item)
-                        row_num+=1
-                    for table_item in sec_question['option']:
-                        print(table_item)
-                        ws.write(row_num,line_num,table_item)
-                        row_num+=1
-                row_num+=1
-                for filled_q in self.filled_question_qid[sec_question['qid']]:
-                    ws.write(row_num,line_num,filled_q[4])
-                    ws.write(row_num, line_num+1,filled_q[5])
-                    row_num += 1
-                line_num=line_num+2
-        wb.save(save_path)
-        logging.info("Success export to {}".format(save_path))
+                    ws.write(row_num,line_num,sec_question['title'])
+                    row_num+=1
+                    if sec_question['type']=='table':
+                        table_content=self.filled_question_qid[sec_question['qid']]
+                        if len(table_content)>0:
+                            table_content_str=table_content[0][5]
+                            table_content_row_list=table_content_str[2:-3].split('],[')
+
+
+                            for row_index in range(len(table_content_row_list)):
+                                table_content_line_list=table_content_row_list[row_index].split(',')
+                                for line_index in range(len(table_content_line_list)):
+                                    ws.write(row_num+row_index,line_num+line_index,table_content_line_list[line_index])
+                        else:
+                            for table_item in sec_question['options']:
+                                print(table_item)
+                                ws.write(row_num,line_num,table_item)
+                                line_num+=1
+                            line_num=0
+                            row_num+=1
+                            for table_item in sec_question['extras']:
+                                print(table_item)
+                                ws.write(row_num,line_num,table_item)
+                    elif sec_question['type']=='single':
+                        for filled_q in self.filled_question_qid[sec_question['qid']]:
+                            option_val=sec_question['options'][filled_q[4]]
+                            ws.write(row_num, line_num, option_val)
+                            row_num += 1
+                        line_num=line_num+1
+                    elif sec_question['type']=='multi':
+                        for filled_q in self.filled_question_qid[sec_question['qid']]:
+                            option_val=""
+                            for bin_index in range(len(sec_question['options'])):
+                                if (filled_q[4]&(2**bin_index))!=0:
+                                    option_val+=sec_question['options'][bin_index]+','
+                            ws.write(row_num, line_num, option_val)
+                            row_num += 1
+                        line_num=line_num+1
+
+                        pass
+                    else:
+                        for filled_q in self.filled_question_qid[sec_question['qid']]:
+                            ws.write(row_num, line_num,filled_q[5])
+                            row_num += 1
+                        line_num=line_num+1
+            wb.save(save_path)
+            logging.info("Success export to {}".format(save_path))
+        except AttributeError as e:
+            print(e)
+            logging.info("{} in save_form".format(e))
+
+
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import configparser
+import configparser,sys
 from FilledForm import *
 
 
 
 if __name__=='__main__':
-    filled_from_id_list=[66]
     config = configparser.ConfigParser()
     config.read('config.ini')
     database_info=config['DATABASE']
@@ -16,12 +15,34 @@ if __name__=='__main__':
 
 
 
-    filled_form=FilledForm(filled_from_id_list[0])
-    filled_form.init_form(database_info,table_name)
+    if len(sys.argv)<2:
+        print('Error argv missing form id list')
+        logging.info('Error argv missing form id list')
+    else:
+        filled_arg=sys.argv[1]
+        export_arg=sys.argv[2]
 
-    for from_id in filled_from_id_list:
-        filled_form.add_form(from_id,database_info,table_name)
-    filled_form.save_form('example.xls')
+        filled_from_id_list=filled_arg.split('=')[1].split(',')
+        export_path=export_arg.split('=')[1]
+
+        filled_from_id_list=[index for index in range(85,193)]
+
+        try:
+            filled_form=FilledForm(filled_from_id_list[0])
+            filled_form.init_form(database_info,table_name)
+            if filled_form==None:
+                print("Failed to init form")
+                logging.info("Failed to init form")
+            else:
+                for from_id in filled_from_id_list:
+                    filled_form.add_form(from_id,database_info,table_name)
+                filled_form.save_form(export_path)
+                print("Success export form to {}".format(export_path))
+                logging.info("Success export form to {}".format(export_path))
+        except:
+            print("Failed to export form")
+            logging.info("Failed to export form")
+
 
 
 
